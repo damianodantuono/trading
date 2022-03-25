581ca3faad3f6ce9e00aa390d2118d249bce9d64
Damiano D'Antuono <damianodantuono95@gmail.com>
2022-03-25 12:54:42 +0100

    wrtiting stoploss

--- trading/Indicators.py
+++ trading/Indicators.py
@@ -7,16 +7,10 @@ def apply_function_to_series(series, periods, function):
 
 
 def crossunder(array1, array2):
-    """
-    when array1 crosses top-to-bottom array2, the function returns true
-    """
     return (array1 < array2) & (array1.shift(1) > array2.shift(2))
 
 
 def crossover(array1, array2):
-    """
-    when array1 crosses bottom-to-top array2, the function returns true
-    """
     return (array1 > array2) & (array1.shift(1) < array2.shift(2))
 
 
@@ -34,3 +28,11 @@ def add_standard_deviation(close_series, periods):
 
 def add_mean(close_series, periods):
     return apply_function_to_series(close_series, periods, np.mean)
+
+
+def add_stoploss(entry_level: pd.Series, enter_rule: pd.Series, exit_rule: pd.Series, stop_loss: float) -> pd.Series:
+    df = pd.DataFrame({'entry_level': entry_level, 'enter_rule': enter_rule, 'exit_rule': exit_rule})
+    df = df.sort_index()
+    for index, row in df.iterrows():
+        if row['enter_rule']:
+            pass
