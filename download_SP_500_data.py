959a88884db9ebe2ce355a4507325af415132ff0
Damiano D'Antuono <damianodantuono95@gmail.com>
2022-04-29 10:15:37 +0200

    REMOVED .ENV

--- download_SP_500_data.py
+++ download_SP_500_data.py
@@ -2,11 +2,8 @@ import pandas as pd
 import os
 from trading.Stock import Stock
 import parameters
-import dotenv
 
-dotenv.load_dotenv()
-
-tickers = pd.read_html(os.getenv("SP500"))[0]["Symbol"].values.tolist()
+tickers = pd.read_html(parameters.SP500)[0]["Symbol"].values.tolist()
 
 for i, ticker in enumerate(tickers):
     complete = round(i/len(tickers) * 100, 2)
--- parameters.py
+++ parameters.py
@@ -4,3 +4,4 @@ money = 10000
 fees = 0.1
 tick = 0.01
 start = '2000-01-01'
+SP500 = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
