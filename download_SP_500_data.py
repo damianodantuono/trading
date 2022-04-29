ef887f89a3c04bd3d155a12d6c04551a52235f95
Damiano D'Antuono <damianodantuono95@gmail.com>
2022-04-29 10:04:07 +0200

    added requirements.txt

--- download_SP_500_data.py
+++ download_SP_500_data.py
@@ -3,14 +3,12 @@ import os
 from trading.Stock import Stock
 import parameters
 import dotenv
-import time
 
 dotenv.load_dotenv()
 
 tickers = pd.read_html(os.getenv("SP500"))[0]["Symbol"].values.tolist()
 
-for i, ticker in enumerate(tickers[:1]):
-    ticker = "AAPL"
+for i, ticker in enumerate(tickers):
     complete = round(i/len(tickers) * 100, 2)
     print("Downloading", ticker, complete, "%")
     try:
